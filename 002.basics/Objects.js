ce674156bafadfbc28b1c67efb6ad5e133814c9f
mohit <mohitrakheja007@gmail.com>
2023-11-29 01:00:03 +0530

    objects basics

---
+++ 002.basics/Objects.js
@@ -0,0 +1,13 @@
+"use strict";
+// const User = {
+//     name: "hitesh",
+//     email: "hitesh@lco.dev",
+//     isAvtive: true
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+var cd = {
+    cardnumber: "abc",
+    cardDate: "def",
+    cvv: 123
+};
+console.log(cd);
