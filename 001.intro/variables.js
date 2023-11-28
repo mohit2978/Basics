32be345c4cb0f597ce31d1480f6db53a0cc2cbff
mohit <mohitrakheja007@gmail.com>
2023-11-28 19:50:32 +0530

    variables

---
+++ 001.intro/variables.js
@@ -0,0 +1,6 @@
+var greet = "Hello Mohit";
+console.log(greet);
+//number
+var i = 554441;
+var u = 123.3211;
+var check = true;
---
+++ 001.intro/variables.ts
@@ -0,0 +1,19 @@
+let greet:string="Hello Mohit";
+console.log(greet);
+
+
+
+//number
+
+let i:number=554441;
+
+let u:number=123.3211;
+
+let check:boolean=true;
+
+let hero;
+
+function gethero() {
+    return "hero";
+}
+hero=gethero();
